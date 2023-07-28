f3f6371e91d2112b7fa685aaa1ddf6bab2a66066
ibvwvq <ikramova_arina@mail.ru>
2023-07-29 01:22:49 +0300

    метод получения всех клиентов

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MainController extends Controller
 {
-    //
+    public function getClients(){
+        $clients = DB::table('clients')->paginate(3);
+        return view('welcome',['clients'=>$clients]);
+    }
 }
