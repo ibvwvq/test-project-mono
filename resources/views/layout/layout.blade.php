4f82f471c28b408ca59e31f4fda7862fcdba5b5a
ibvwvq <ikramova_arina@mail.ru>
2023-07-29 01:12:41 +0300

    интерфейс страницы с layout

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -1,1 +1,33 @@
-<?php
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+        @yield('title')
+    </title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body class="bg-light text-black" >
+<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+        <span class="fs-4">Автостоянка</span>
+    </a>
+
+    <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link">Клиенты</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Автомобили</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Отзывы</a></li>
+    </ul>
+</header>
+
+<div class="container">
+    @yield('content')
+</div>
+
+</body>
+</html>
+
